1985be455f88170d4ee02a6cb8c80217ad11beaa
Jo du Plessis <jo@joduplessis.com>
2020-06-10 08:33:53 +0200

    Fixed no return

--- sqs/receiver.ts
+++ sqs/receiver.ts
@@ -1,23 +1,24 @@
-import { SQSHandler, SQSMessageAttributes } from 'aws-lambda';
+import { SQSHandler, SQSMessageAttributes, SQSEvent } from 'aws-lambda';
 import axios from 'axios'
 
-const receiver: SQSHandler = async (event) => {
-  try {
-    for (const record of event.Records) {
-      console.log('ATTR: ', record.messageAttributes);
-      console.log('BODY: ', record.body);
+const receiver: SQSHandler = async (event: SQSEvent): Promise<any> => {
+  console.log('SQSHandler Invoked')
 
-      const messageAttributes: SQSMessageAttributes = record.messageAttributes;
-      const outgoing: string = messageAttributes['outgoing'].stringValue;
-      const token: string = messageAttributes['token'].stringValue;
-      const payload: any = JSON.parse(messageAttributes['payload'].stringValue);
+  for (const record of event.Records) {
+    console.log('ATTR: ', record.messageAttributes);
+    console.log('BODY: ', record.body);
+    console.log('SQSHandler messageId: ', record.messageId);
 
-      await axios.post(`${outgoing}?token=${token}`, payload, {
-        headers: { 'Content-Type': 'application/json' },
-      })
-    }
-  } catch (error) {
-    console.log(error);
+    const messageAttributes: SQSMessageAttributes = record.messageAttributes;
+    const outgoing: string = messageAttributes['outgoing'].stringValue;
+    const token: string = messageAttributes['token'].stringValue;
+    const payload: any = JSON.parse(messageAttributes['payload'].stringValue);
+
+    const promise = axios.post(`${outgoing}?token=${token}`, payload, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    return promise
   }
 };
 
